f5d9dd073b6452bad8812a1cfd2e68dc28ee24aa
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2025-12-07 14:46:31 -0500

    Update NTP Name and Service.ps1 to version 1.5. Added Jonathan Pitre as an author and updated the NTP server list to include 4 US-based servers for improved accuracy.

--- Plugins/30 Host/31 NTP Name and Service.ps1
+++ Plugins/30 Host/31 NTP Name and Service.ps1
@@ -2,8 +2,8 @@ $Title = "NTP Name and Service"
 $Header = "NTP Issues: [count]"
 $Comments = "The following hosts do not have the correct NTP settings and may cause issues if the time becomes far apart from the vCenter/Domain or other hosts"
 $Display = "Table"
-$Author = "Alan Renouf, Dan Barr, Bill Wall"
-$PluginVersion = 1.4
+$Author = "Alan Renouf, Dan Barr, Bill Wall, Jonathan Pitre"
+$PluginVersion = 1.5
 $PluginCategory = "vSphere"
 
 # Start of Settings
@@ -16,3 +16,4 @@ $VMH | Where-Object { $_.ConnectionState -match "Connected|Maintenance" } | Sele
 # Changelog
 ## 1.3 : Only check Connected hosts since Disconnected and Not Responding produce empty data
 ## 1.4 : Added check for Maintenance mode VMHs
+## 1.5 : Updated NTP server list to include 4 US-based servers instead
