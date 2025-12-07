fb9fdfd613b5fb6e27d6cb7adf5607210d8a0a4e
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2025-12-07 12:37:46 -0500

    Update Find VMs in Uncontrolled Snapshot Mode.ps1 to version 1.7. Optimized the script by utilizing the vSphere API's SearchDatastoreSubFolders for improved performance, significantly reducing execution time from hours to minutes. Added an exception for specific file patterns.

--- Plugins/60 VM/79 Find VMs in Uncontrolled Snapshot Mode.ps1
+++ Plugins/60 VM/79 Find VMs in Uncontrolled Snapshot Mode.ps1
@@ -14,43 +14,87 @@ $Title = "VMs in uncontrolled snapshot mode"
 $Header = "VMs in uncontrolled snapshot mode: [count]"
 $Comments = "The following VMs are in snapshot mode, but vCenter isn't aware of it. See http://kb.vmware.com/kb/1002310"
 $Display = "Table"
-$Author = "Rick Glover, Matthias Koehler, Dan Rowe, Bill Wall"
-$PluginVersion = 1.6
+$Author = "Rick Glover, Matthias Koehler, Dan Rowe, Bill Wall, Legion87"
+$PluginVersion = 1.7
 $PluginCategory = "vSphere"
 
 # Start of Settings
 # Do not report uncontrolled snapshots on VMs that are defined here
 $ExcludeDS = "ExcludeMe"
+# Exception for specific file patterns (e.g., Xen-Desktop: "-xd-|-ctk.")
+$ExceptionFP = ""
 # End of Settings
 
-$i=0;
-foreach ($eachDS in ($Datastores | Where-Object {$_.Name -notmatch $ExcludeDS} | Where-Object {$_.State -eq "Available"})) {
-   Write-Progress -ID 2 -Parent 1 -Activity $pLang.pluginActivity -Status ($pLang.pluginStatus -f $i, $Datastores.count, $eachDS.Name) -PercentComplete ($i*100/$Datastores.count)
-   
-   $FilePath = $eachDS.DatastoreBrowserPath + '\*\*delta.vmdk*'
-   $fileList = @(Get-ChildItem -Path "$FilePath" | Select-Object Name, FolderPath, FullName)
-   $FilePath = $eachDS.DatastoreBrowserPath + '\*\-*-flat.vmdk'
-   $fileList += Get-ChildItem -Path "$FilePath" | Select-Object Name, FolderPath, FullName
+# Variables for the results
+$vmdks = @()
+$items = @()
 
-   $i++
+$i = 0
+$datastoresToProcess = $Datastores | Where-Object {$_.Name -notmatch $ExcludeDS -and $_.State -eq "Available"}
+foreach ($Datastore in $datastoresToProcess) {
+    Write-Progress -ID 2 -Parent 1 -Activity $pLang.pluginActivity -Status ($pLang.pluginStatus -f $i, $datastoresToProcess.count, $Datastore.Name) -PercentComplete ($i*100/$datastoresToProcess.count)
 
-   foreach ($vmFile in $filelist | Sort-Object FolderPath) 
-   {
-      $vmFile.FolderPath -match '^\[([^\]]+)\] ([^/]+)' > $null
-      $VMName = $matches[2]
-      $eachVM = $FullVM | Where-Object {$_.Name -eq $VMName}
-      if (!$eachVM.snapshot) 
-      { 
-         # Only process VMs without snapshots
-         New-Object -TypeName PSObject -Property @{
-            VM = $eachVM.Name
-            Datacenter = $eachDS.Datacenter
-            Path = $vmFile.FullName
-         }
-      }
-   }
+    $DatastoreBrowser = Get-View $Datastore.ExtensionData.Browser
+
+    # Determine all visible folders in the datastore
+    $Folders = Get-ChildItem -Path $Datastore.DatastoreBrowserPath -Force | Where-Object {
+        # Do not search hidden folders
+        $_.PSIsContainer -and $_.Name -notmatch '^\.'
+    } | Sort-Object Name
+
+    foreach ($Folder in $Folders) {
+        # Create DatastorePath for each folder
+        $DatastorePath = "[" + $Datastore.Name + "] /" + $Folder.Name
+
+        # Define search patterns
+        $SearchSpec = New-Object VMware.Vim.HostDatastoreBrowserSearchSpec
+        # Search for all .vmdk files
+        $SearchSpec.MatchPattern = @("*.vmdk")
+        $SearchSpec.SearchCaseInsensitive = $true
+
+        # Start search in specific folder using API
+        $SearchResults = $DatastoreBrowser.SearchDatastoreSubFolders($DatastorePath, $SearchSpec)
+
+        # Collect results
+        foreach ($FolderResult in $SearchResults) {
+            foreach ($File in $FolderResult.File) {
+                # Check if file matches uncontrolled snapshot patterns (delta.vmdk or -flat.vmdk)
+                # and doesn't match exception patterns
+                if (($File.Path -match 'delta\.vmdk' -or $File.Path -match '-flat\.vmdk') -and
+                    ($ExceptionFP -eq "" -or $File.Path -notmatch $ExceptionFP)) {
+                    $items += [PSCustomObject]@{
+                        Fullname   = $Datastore.DatastoreBrowserPath + '\' + $Folder.Name + '\' + $File.Path
+                        FolderPath = $FolderResult.FolderPath.TrimEnd('/')
+                        Datacenter = $Datastore.Datacenter
+                    }
+                }
+            }
+        }
+    }
+
+    $i++
 }
+
+# Process collected results
+foreach ($vmFile in ($items | Sort-Object FolderPath)) {
+    $vmFile.FolderPath -match '^\[([^\]]+)\] ([^/]+)' > $null
+    $VMName = $matches[2]
+    $eachVM = $FullVM | Where-Object {$_.Name -eq $VMName}
+    if (!$eachVM.snapshot) {
+        # Only process VMs without snapshots
+        $vmdks += New-Object -TypeName PSObject -Property @{
+            VM = $eachVM.Name
+            Datacenter = $vmFile.Datacenter
+            Path = $vmFile.Fullname
+        }
+    }
+}
+
 Write-Progress -ID 1 -Activity $pLang.pluginActivity -Status $pLang.Complete -Completed
+
+# Output results
+$vmdks
 
 # Changelog
 ## 1.6 : Added setting to exclude DS
+## 1.7 : Optimized using vSphere API SearchDatastoreSubFolders for significant performance improvement (reduces execution time from hours to minutes)
