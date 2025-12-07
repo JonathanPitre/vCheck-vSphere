e73dbe3cda4cae5bda898e5a2027e5b377ae1d1c
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2025-12-07 12:37:16 -0500

    Update 60 Powered Off VMs.ps1 to version 1.7. Improved memory efficiency by filtering VMs before checking LastPoweredOffDate, addressing issue #765.

--- Plugins/60 VM/60 Powered Off VMs.ps1
+++ Plugins/60 VM/60 Powered Off VMs.ps1
@@ -2,10 +2,10 @@ $Title = "Powered Off VMs"
 $Header = "VMs Powered Off - Number of Days"
 $Display = "Table"
 $Author = "Adam Schwartzberg, Fabio Freire"
-$PluginVersion = 1.6
+$PluginVersion = 1.7
 $PluginCategory = "vSphere"
 
-# Start of Settings 
+# Start of Settings
 # VMs not to report on (regex)
 $IgnoredVMs = "TEMPLATE|BUILD"
 # VmPathName not to report on
@@ -21,18 +21,43 @@ $IgnoredVMs = Get-vCheckSetting $Title "IgnoredVMs" $IgnoredVMs
 $IgnoredVMpath = Get-vCheckSetting $Title "IgnoredVMpath" $IgnoredVMpath
 $PoweredOffDays = Get-vCheckSetting $Title "PoweredOffDays" $PoweredOffDays
 
-$VM | Where-Object {$_.ExtensionData.Config.ManagedBy.ExtensionKey -ne 'com.vmware.vcDr' -and 
-                $_.PowerState -eq "PoweredOff" -and 
-                $_.LastPoweredOffDate -lt $date.AddDays(-$PoweredOffDays) -and
-                $_.Name -notmatch $IgnoredVMs -and 
-                $_.Folder.Name -notmatch $IgnoredVMFolder -and
-                $_.ExtensionData.Config.Files.VmPathName -notmatch $IgnoredVMpath} |
-  Select-Object -Property Name, LastPoweredOffDate, @{l = 'Folder'; e = {$_.Folder.Name}}, Notes |
-  Sort-Object -Property LastPoweredOffDate
+# Filter VMs first using cheap properties to avoid expensive LastPoweredOffDate queries
+# This prevents memory issues when there are many powered-off VMs
+$filteredVMs = $VM | Where-Object {
+    $_.ExtensionData.Config.ManagedBy.ExtensionKey -ne 'com.vmware.vcDr' -and
+    $_.PowerState -eq "PoweredOff" -and
+    $_.Name -notmatch $IgnoredVMs -and
+    $_.Folder.Name -notmatch $IgnoredVMFolder -and
+    $_.ExtensionData.Config.Files.VmPathName -notmatch $IgnoredVMpath
+}
+
+# Now check LastPoweredOffDate only for filtered VMs to minimize expensive event queries
+$results = @()
+foreach ($vm in $filteredVMs) {
+    try {
+        $lastPoweredOffDate = $vm.LastPoweredOffDate
+        if ($lastPoweredOffDate -and $lastPoweredOffDate -lt $date.AddDays(-$PoweredOffDays)) {
+            $results += [PSCustomObject]@{
+                Name = $vm.Name
+                LastPoweredOffDate = $lastPoweredOffDate
+                Folder = $vm.Folder.Name
+                Notes = $vm.Notes
+            }
+        }
+    }
+    catch {
+        # Skip VMs where LastPoweredOffDate query fails to prevent script failure
+        Write-Verbose "Could not get LastPoweredOffDate for VM $($vm.Name): $_"
+    }
+}
+
+# Output sorted results
+$results | Sort-Object -Property LastPoweredOffDate
 
 $Comments = ("May want to consider deleting VMs that have been powered off for more than {0} days" -f $PoweredOffDays)
 
-# Change Log 
-## 1.4 : Added Get-vCheckSetting, $PoweredOffDays 
+# Change Log
+## 1.4 : Added Get-vCheckSetting, $PoweredOffDays
 ## 1.5 : Select-Object now returns Folder as a string; Added IgnoredVMpath
 ## 1.6 : Added IgnoredVMFolder
+## 1.7 : Fixed memory issue by filtering VMs first, then checking LastPoweredOffDate only for filtered VMs (Issue #765)
