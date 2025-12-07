e9c90e13244aadd489f67a70db87292fc6500fd8
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2025-12-07 16:13:02 -0500

    Update VM CPU %RDY.ps1 to version 1.3. Revised documentation links to point to the official Broadcom Knowledge Base article and VMware Performance Best Practices guide for enhanced user guidance. Improved code formatting for consistency.

--- Plugins/60 VM/32 VM CPU Percent RDY.ps1
+++ Plugins/60 VM/32 VM CPU Percent RDY.ps1
@@ -1,8 +1,8 @@
 $Title = "VM CPU %RDY"
-$Comments = "The following VMs have high CPU RDY times, this can cause performance issues for more information please read <a href='http://communities.vmware.com/docs/DOC-7390' target='_blank'>This article</a>"
+$Comments = "The following VMs have high CPU RDY times, this can cause performance issues. For more information see <a href='https://knowledge.broadcom.com/external/article/387750' target='_blank'>Understanding the CPU Ready values in the vSphere Client</a> and <a href='https://www.vmware.com/docs/vsphere-esxi-vcenter-server-80-performance-best-practices' target='_blank'>Performance Best Practices for VMware vSphere 8.0</a>"
 $Display = "Table"
 $Author = "Alan Renouf"
-$PluginVersion = 1.2
+$PluginVersion = 1.3
 $PluginCategory = "vSphere"
 
 # Start of Settings 
@@ -23,18 +23,17 @@ Import-LocalizedData -BaseDirectory ($ScriptPath + "\Lang") -BindingVariable pLa
 # Update settings where there is an override
 $PercCPUReady = Get-vCheckSetting $Title "PercCPUReady" $PercCPUReady
 
-$i=0
-ForEach ($v in ($VM | Where-Object {$_.PowerState -eq "PoweredOn"})){
-   Write-Progress -ID 2 -Parent 1 -Activity $plang.pluginActivity -Status $v.Name -PercentComplete ((100*$i)/$VM.Count)
-   For ($cpunum = 0; $cpunum -lt $v.NumCpu; $cpunum++){
-      $PercReady = [Math]::Round((($v | Get-Stat -ErrorAction SilentlyContinue -Stat Cpu.Ready.Summation -Realtime | Where-Object {$_.Instance -eq $cpunum} | Measure-Object -Property Value -Average).Average)/200,1)
+$i = 0
+ForEach ($v in ($VM | Where-Object { $_.PowerState -eq "PoweredOn" })) {
+   Write-Progress -ID 2 -Parent 1 -Activity $plang.pluginActivity -Status $v.Name -PercentComplete ((100 * $i) / $VM.Count)
+   For ($cpunum = 0; $cpunum -lt $v.NumCpu; $cpunum++) {
+      $PercReady = [Math]::Round((($v | Get-Stat -ErrorAction SilentlyContinue -Stat Cpu.Ready.Summation -Realtime | Where-Object { $_.Instance -eq $cpunum } | Measure-Object -Property Value -Average).Average) / 200, 1)
       
-      if ($PercReady -gt $PercCPUReady)
-      {
+      if ($PercReady -gt $PercCPUReady) {
          New-Object -TypeName PSObject -Property @{
-            VM = $v.Name
-            VMHost = $v.VMHost
-            CPU = $cpunum
+            VM        = $v.Name
+            VMHost    = $v.VMHost
+            CPU       = $cpunum
             PercReady = $PercReady
          }
       }
@@ -47,3 +46,4 @@ $Header = ("VM CPU % RDY over {0}: [count]" -f $PercCPUReady)
 
 # Change Log
 ## 1.2 :  Added Get-vCheckSetting, code refactor
+## 1.3 : Updated broken documentation link to official Broadcom Knowledge Base article and Performance Best Practices guide
