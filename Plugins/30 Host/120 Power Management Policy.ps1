4e3378344b1263c3afdb36131ceb14b53620fc88
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2025-12-07 14:46:50 -0500

    Update Power Management Policy.ps1 to version 1.1. Updated the power management policy documentation link to VMware vSphere 8.0, modified the default power policy to 'static' for improved performance, and added Jonathan Pitre as a co-author.

--- Plugins/30 Host/120 Power Management Policy.ps1
+++ Plugins/30 Host/120 Power Management Policy.ps1
@@ -1,20 +1,20 @@
 $Title = "Host Power Management Policy"
-$Comments = "The following hosts are not using the specified power management policy.  Power management may impact performance for latency sensitive workloads.  For details see <a href='https://www.vmware.com/content/dam/digitalmarketing/vmware/en/pdf/techpaper/performance/Perf_Best_Practices_vSphere65.pdf' >Performance Best Practices for VMware vSphere 6.5</a>"
+$Comments = "The following hosts are not using the specified power management policy.  Power management may impact performance for latency sensitive workloads.  For details see <a href='https://www.vmware.com/docs/vsphere-esxi-vcenter-server-80-performance-best-practices' >Performance Best Practices for VMware vSphere 8.0</a>"
 $Display = "Table"
-$Author = "Doug Taliaferro"
-$PluginVersion = 1.0
+$Author = "Doug Taliaferro & Jonathan Pitre"
+$PluginVersion = 1.1
 $PluginCategory = "vSphere"
 
 # Start of Settings
-# Which power management policy should your hosts use? For Balanced enter "dynamic" (this is the ESXi default policy), for High Performance enter "static", for Low power enter "low".
-$PowerPolicy = "dynamic"
+# Which power management policy should your hosts use? For High Performance enter "static" (recommended for 2025 - ensures maximum performance for latency-sensitive workloads), for Balanced enter "dynamic" (this is the ESXi default policy), for Low power enter "low".
+$PowerPolicy = "static"
 # End of Settings
 
 # Update settings where there is an override
 $PowerPolicy = Get-vCheckSetting $Title "PowerPolicy" $PowerPolicy
 
 $hostResults = @()
-Foreach ($esxhost in ($HostsViews | Where-Object {$_.Runtime.ConnectionState -match "Connected|Maintenance"})) {
+Foreach ($esxhost in ($HostsViews | Where-Object { $_.Runtime.ConnectionState -match "Connected|Maintenance" })) {
     If ($esxhost.config.PowerSystemInfo.CurrentPolicy.ShortName -ne $PowerPolicy) {
         $myObj = "" | Select-Object VMHost, PowerPolicy
         $myObj.VMHost = $esxhost.Name
