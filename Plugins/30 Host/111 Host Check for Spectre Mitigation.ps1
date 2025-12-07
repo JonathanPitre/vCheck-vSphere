4b8700a709570b921dad9d3732aa077f254b3e01
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2025-12-07 16:12:07 -0500

    Refactor Spectre Mitigation Check Script for Improved Readability. Updated code formatting for consistency, enhanced comments with accurate links, and filtered results to show only affected ESXi hosts.

--- Plugins/30 Host/111 Host Check for Spectre Mitigation.ps1
+++ Plugins/30 Host/111 Host Check for Spectre Mitigation.ps1
@@ -1,7 +1,7 @@
 # Start of Settings 
 # End of Settings 
 
-$vmhosts = Get-View -ViewType HostSystem -Property Name,Config.FeatureCapability
+$vmhosts = Get-View -ViewType HostSystem -Property Name, Config.FeatureCapability
 
 $result = @()
 foreach ($vmhost in $vmhosts | Sort-Object -Property Name) {
@@ -13,34 +13,36 @@ foreach ($vmhost in $vmhosts | Sort-Object -Property Name) {
 
     $cpuFeatures = $vmhost.Config.FeatureCapability
     foreach ($cpuFeature in $cpuFeatures) {
-        if($cpuFeature.key -eq "cpuid.IBRS" -and $cpuFeature.value -eq 1) {
+        if ($cpuFeature.key -eq "cpuid.IBRS" -and $cpuFeature.value -eq 1) {
             $IBRSPass = $true
-        } elseif($cpuFeature.key -eq "cpuid.IBPB" -and $cpuFeature.value -eq 1) {
+        } elseif ($cpuFeature.key -eq "cpuid.IBPB" -and $cpuFeature.value -eq 1) {
             $IBPBPass = $true
-        } elseif($cpuFeature.key -eq "cpuid.STIBP" -and $cpuFeature.value -eq 1) {
+        } elseif ($cpuFeature.key -eq "cpuid.STIBP" -and $cpuFeature.value -eq 1) {
             $STIBPPass = $true
         }
     }
 
     $vmhostAffected = $true
-    if($IBRSPass -or $IBPBPass -or $STIBPass) {
+    if ($IBRSPass -or $IBPBPass -or $STIBPass) {
         $vmhostAffected = $false
     }
 
     $tmp = [pscustomobject] @{
-        VMHost = $vmhostDisplayName;
-        IBRPresent = $IBRSPass;
-        IBPBPresent = $IBPBPass;
-        STIBPresent = $STIBPPass;
-        Affected = $vmhostAffected;
+        VMHost      = $vmhostDisplayName
+        IBRPresent  = $IBRSPass
+        IBPBPresent = $IBPBPass
+        STIBPresent = $STIBPPass
+        Affected    = $vmhostAffected
     }
-    $result+=$tmp
+    $result += $tmp
 }
+# Filter to only show affected hosts
+$Result = $result | Where-Object { $_.Affected -eq $true }
 $Result
 
 $Title = "ESXi Hosts Exposed to Spectre Vulnerability"
 $Header = "ESXi Hosts Exposed to Spectre Vulnerability: $(@($Result).count)"
-$Comments = "The following ESXi Hosts require remediation to mitigate the Spectre vulnerability. See the following URLs for more information: <a href='https://kb.vmware.com/s/article/52085' target='_blank'>KB 52085</a>, <a href='https://www.virtuallyghetto.com/2018/01/verify-hypervisor-assisted-guest-mitigation-spectre-patches-using-powercli.html' target='_blank'>Virtually Ghetto</a>."
+$Comments = "The following ESXi Hosts require remediation to mitigate the Spectre vulnerability. See the following URLs for more information: <a href='https://knowledge.broadcom.com/external/article?legacyId=52085' target='_blank'>KB 52085</a>, <a href='https://www.virtuallyghetto.com/2018/01/verify-hypervisor-assisted-guest-mitigation-spectre-patches-using-powercli.html' target='_blank'>Virtually Ghetto</a>."
 $Display = "Table"
 $Author = "William Lam"
 $PluginVersion = 1.0
