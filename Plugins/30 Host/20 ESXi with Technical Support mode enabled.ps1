d1a49726bc6710471b7cc12524ce14e70f051832
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2025-12-07 16:11:11 -0500

    Update ESXi with Technical Support mode enabled.ps1 to version 1.4. Revised comments to include updated links to the vSphere Security Configuration Guide and Performance Best Practices for VMware vSphere 8.0. Improved code readability by standardizing formatting in the script.

--- Plugins/30 Host/20 ESXi with Technical Support mode enabled.ps1
+++ Plugins/30 Host/20 ESXi with Technical Support mode enabled.ps1
@@ -1,22 +1,22 @@
 $Title = "ESXi with Technical Support mode or ESXi Shell enabled"
 $Header = "ESXi Hosts with Tech Support Mode or ESXi Shell Enabled : [count]"
-$Comments = "The following ESXi Hosts have Technical support mode or ESXi Shell enabled, this may not be the best security option, see here for more information: <a href='http://www.yellow-bricks.com/2010/03/01/disable-tech-support-on-esxi/' target='_blank'>Yellow-Bricks Disable Tech Support on ESXi</a>."
+$Comments = "The following ESXi Hosts have Technical support mode or ESXi Shell enabled, this may not be the best security option. For more information see <a href='https://www.vmware.com/docs/vsphere-esxi-vcenter-server-80-security-configuration-guide' target='_blank'>vSphere Security Configuration Guide</a> and <a href='https://www.vmware.com/docs/vsphere-esxi-vcenter-server-80-performance-best-practices' target='_blank'>Performance Best Practices for VMware vSphere 8.0</a>."
 $Display = "Table"
 $Author = "Alan Renouf"
-$PluginVersion = 1.3
+$PluginVersion = 1.4
 $PluginCategory = "vSphere"
 
 # Start of Settings 
 # End of Settings 
 
 $VMH | Where-Object { ($_.Version -lt 4.1) -and 
-                      ($_.ConnectionState -in @("Connected","Maintenance")) -and 
-                      ($_.ExtensionData.Summary.Config.Product.Name -match "i")} | 
-       Select-Object Name, @{N="TechSupportModeEnabled";E={($_ | Get-AdvancedSetting -Name VMkernel.Boot.techSupportMode).value}} | 
-       Where-Object { $_.TechSupportModeEnabled -eq $true }
+       ($_.ConnectionState -in @("Connected", "Maintenance")) -and 
+       ($_.ExtensionData.Summary.Config.Product.Name -match "i") } | 
+Select-Object Name, @{N = "TechSupportModeEnabled"; E = { ($_ | Get-AdvancedSetting -Name VMkernel.Boot.techSupportMode).value } } | 
+Where-Object { $_.TechSupportModeEnabled -eq $true }
 
 $VMH | Where-Object { ($_.Version -ge "4.1.0") -and 
-                      ($_.ConnectionState -in @("Connected","Maintenance"))} | 
-       Select-Object Name, @{N="TechSupportModeEnabled";E={($_ | Get-VMHostService | Where-Object {$_.key -eq "TSM"}).Running}} | 
-       Where-Object { $_.TechSupportModeEnabled -eq $true }
+       ($_.ConnectionState -in @("Connected", "Maintenance")) } | 
+Select-Object Name, @{N = "TechSupportModeEnabled"; E = { ($_ | Get-VMHostService | Where-Object { $_.key -eq "TSM" }).Running } } | 
+Where-Object { $_.TechSupportModeEnabled -eq $true }
        
