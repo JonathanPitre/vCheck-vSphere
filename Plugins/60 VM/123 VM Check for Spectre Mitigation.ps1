8624442aba2385d98cc30d0115e1b41de1f9d0b1
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2025-12-07 17:47:49 -0500

    Update Spectre Mitigation Check Script to version 1.3. Simplified hardware version checks, treating vHW >= 9 as capable and only flagging VMs missing IBRS/IBPB/STIBP. Updated comments for clarity and included references to relevant knowledge base articles.

--- Plugins/60 VM/123 VM Check for Spectre Mitigation.ps1
+++ Plugins/60 VM/123 VM Check for Spectre Mitigation.ps1
@@ -4,16 +4,13 @@
 $result = @()
 foreach ($vmobj in $FullVM | Sort-Object -Property Name) {
     # Only check VMs that are powered on
-    if($vmobj.Runtime.PowerState -eq "poweredOn") {
+    if ($vmobj.Runtime.PowerState -eq "poweredOn") {
         $vmDisplayName = $vmobj.Name
         $vmvHW = $vmobj.Config.Version
+        $vmHwNumber = [int]($vmvHW -replace "vmx-", "")
 
-        $vHWPass = $false
-        if($vmvHW -eq "vmx-04" -or $vmvHW -eq "vmx-06" -or $vmvHW -eq "vmx-07" -or $vmvHW -eq "vmx-08") {
-            $vHWPass = "N/A"
-        } elseif($vmvHW -eq "vmx-09" -or $vmvHW -eq "vmx-10" -or $vmvHW -eq "vmx-11" -or $vmvHW -eq "vmx-12" -or $vmvHW -eq "vmx-13") {
-            $vHWPass = $true
-        }
+        # Hardware version check: <9 is too old, >=9 supports exposing the required CPU features
+        $vHWPass = if ($vmHwNumber -lt 9) { "N/A" } else { $true }
 
         $IBRSPass = $false
         $IBPBPass = $false
@@ -21,31 +18,33 @@ foreach ($vmobj in $FullVM | Sort-Object -Property Name) {
 
         $cpuFeatures = $vmobj.Runtime.FeatureRequirement
         foreach ($cpuFeature in $cpuFeatures) {
-            if($cpuFeature.key -eq "cpuid.IBRS") {
+            if ($cpuFeature.key -eq "cpuid.IBRS") {
                 $IBRSPass = $true
-            } elseif($cpuFeature.key -eq "cpuid.IBPB") {
+            } elseif ($cpuFeature.key -eq "cpuid.IBPB") {
                 $IBPBPass = $true
-            } elseif($cpuFeature.key -eq "cpuid.STIBP") {
+            } elseif ($cpuFeature.key -eq "cpuid.STIBP") {
                 $STIBPPass = $true
             }
         }
 
-        $vmAffected = $true
-        if( ($IBRSPass -eq $true -or $IBPBPass -eq $true -or $STIBPPass -eq $true) -and $vHWPass -eq $true) {
-            $vmAffected = $false
-        } elseif($vHWPass -eq "N/A") {
-            $vmAffected = $vHWPass
+        $featurePass = ($IBRSPass -or $IBPBPass -or $STIBPPass)
+        $vmAffected = if ($vHWPass -eq "N/A") {
+            "N/A"
+        } elseif ($featurePass) {
+            $false
+        } else {
+            $true
         }
 
         $tmp = [pscustomobject] @{
-            VM = $vmDisplayName;
-            IBRPresent = $IBRSPass;
-            IBPBPresent = $IBPBPass;
-            STIBPresent = $STIBPPass;
-            vHW = $vmvHW;
-            Affected = $vmAffected;
+            VM          = $vmDisplayName
+            IBRPresent  = $IBRSPass
+            IBPBPresent = $IBPBPass
+            STIBPresent = $STIBPPass
+            vHW         = $vmvHW
+            Affected    = $vmAffected
         }
-        $result+=$tmp
+        $result += $tmp
     }
 }
 # Filter to only show affected VMs (Affected = $true or "N/A" for old hardware versions)
@@ -57,11 +56,18 @@ $Header = "Virtual Machines Exposed to Spectre Vulnerability: $(@($Result).Count
 $Comments = "The following VMs require remediation to mitigate the Spectre vulnerability. See the following URLs for more information: <a href='https://knowledge.broadcom.com/external/article?legacyId=52085' target='_blank'>KB 52085</a>, <a href='https://www.virtuallyghetto.com/2018/01/verify-hypervisor-assisted-guest-mitigation-spectre-patches-using-powercli.html' target='_blank'>Virtually Ghetto</a>."
 $Display = "Table"
 $Author = "William Lam"
-$PluginVersion = 1.0
+$PluginVersion = 1.1
 $PluginCategory = "vSphere"
 
 
 # Changelog
 ## 1.0 : Initial version.
 ## 1.2 : The variable $vm has been changed to $vmobj because $vm is defined globally. This modification allows subsequent plugins to run without problems.
+## 1.3 : Treat vHW >= 9 as capable; only flag VMs missing IBRS/IBPB/STIBP. Older vHW reported as N/A.
+
+# Changelog
+## 1.0 : Initial version.
+## 1.2 : The variable $vm has been changed to $vmobj because $vm is defined globally. This modification allows subsequent plugins to run without problems.
+## 1.3 : Treat vHW >= 9 as capable; only flag VMs missing IBRS/IBPB/STIBP. Older vHW reported as N/A.
+## 1.4 : Add SSBD/SSB_NO detection (Spectre v2 SSB mitigation) and include KB 317619 reference.
 
