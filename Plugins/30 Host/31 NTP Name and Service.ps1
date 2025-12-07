4c15e02b003178580b508871de2a510c95c4b807
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2025-12-07 17:33:21 -0500

    Update NTP Name and Service.ps1 to version 1.5. Revised NTP server list to include only 3 US-based servers instead of 4, ensuring accurate configuration for host time synchronization.

--- Plugins/30 Host/31 NTP Name and Service.ps1
+++ Plugins/30 Host/31 NTP Name and Service.ps1
@@ -8,7 +8,7 @@ $PluginCategory = "vSphere"
 
 # Start of Settings
 # The NTP server which should be set on your hosts (comma-separated)
-$ntpserver = "0.us.pool.ntp.org,1.us.pool.ntp.org,2.us.pool.ntp.org,3.us.pool.ntp.org"
+$ntpserver = "0.us.pool.ntp.org,1.us.pool.ntp.org,2.us.pool.ntp.org"
 # End of Settings
 
 $VMH | Where-Object { $_.ConnectionState -match "Connected|Maintenance" } | Select-Object Name, @{N = "NTPServer"; E = { ($_ | Get-VMHostNtpServer) -join "," } }, @{N = "ServiceRunning"; E = { (Get-VmHostService -VMHost $_ | Where-Object { $_.key -eq "ntpd" }).Running } } | Where-Object { $_.ServiceRunning -eq $false -or $_.NTPServer -ne $ntpserver }
@@ -16,4 +16,4 @@ $VMH | Where-Object { $_.ConnectionState -match "Connected|Maintenance" } | Sele
 # Changelog
 ## 1.3 : Only check Connected hosts since Disconnected and Not Responding produce empty data
 ## 1.4 : Added check for Maintenance mode VMHs
-## 1.5 : Updated NTP server list to include 4 US-based servers instead
+## 1.5 : Updated NTP server list to include 3 US-based servers instead
