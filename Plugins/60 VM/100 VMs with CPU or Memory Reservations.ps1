3c0edd9d44841571e78c69aeed571c06af7a5811
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2025-12-07 18:28:32 -0500

    Update 100 VMs with CPU or Memory Reservations.ps1 to version 1.2. Incremented plugin version and modified the script to use ExtensionData.ResourceConfig for CPU and memory reservations, ensuring compatibility with vSphere 8. Null values are now treated as zero for accurate reporting.

--- Plugins/60 VM/100 VMs with CPU or Memory Reservations.ps1
+++ Plugins/60 VM/100 VMs with CPU or Memory Reservations.ps1
@@ -3,7 +3,7 @@ $Header = "VMs with CPU or Memory Reservations Configured: [count]"
 $Comments = "The following VMs have a CPU or Memory Reservation configured which may impact the performance of the VM. Note: -1 indicates no reservation"
 $Display = "Table"
 $Author = "Dan Jellesma"
-$PluginVersion = 1.1
+$PluginVersion = 1.2
 $PluginCategory = "vSphere"
 
 # Start of Settings
@@ -14,8 +14,18 @@ $MCRDoNotInclude = ""
 # Update settings where there is an override
 $MCRDoNotInclude = Get-vCheckSetting $Title "MCRDoNotInclude" $MCRDoNotInclude
 
-$FullVM | Where-Object {$_.Name -notmatch $MCRDoNotInclude -and ($_.config.cpuallocation.Reservation -ne "0" -or $_.config.memoryallocation.Reservation -ne "0")} | Select-Object Name, @{Name="CPUReservationMhz";E={$_.config.cpuallocation.Reservation}}, @{Name="MemReservationMB";E={$_.config.memoryallocation.Reservation}}
+$FullVM |
+Where-Object {
+    $_.Name -notmatch $MCRDoNotInclude -and (
+        [double]($_.ExtensionData.ResourceConfig.CpuAllocation.Reservation ?? 0) -gt 0 -or
+        [double]($_.ExtensionData.ResourceConfig.MemoryAllocation.Reservation ?? 0) -gt 0
+    )
+} |
+Select-Object Name,
+@{Name = "CPUReservationMhz"; E = { $_.ExtensionData.ResourceConfig.CpuAllocation.Reservation } },
+@{Name = "MemReservationMB"; E = { $_.ExtensionData.ResourceConfig.MemoryAllocation.Reservation } }
 
 # Change Log
 ## 1.0 : Initial Release
 ## 1.1 : Added Get-vCheckSetting
+## 1.2 : Use ExtensionData.ResourceConfig* reservations (vSphere 8 compatible) and treat null as 0
