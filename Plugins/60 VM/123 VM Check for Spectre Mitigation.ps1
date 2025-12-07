35f440cc98cbf62998ef9423349b02f0eb8aa640
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2025-12-07 17:54:01 -0500

    Update Spectre Mitigation Check Script to version 1.4. Added detection for SSBD/SSB_NO (Spectre v2 SSB mitigation) and included reference to KB 317619 for enhanced user guidance.

--- Plugins/60 VM/123 VM Check for Spectre Mitigation.ps1
+++ Plugins/60 VM/123 VM Check for Spectre Mitigation.ps1
@@ -56,18 +56,11 @@ $Header = "Virtual Machines Exposed to Spectre Vulnerability: $(@($Result).Count
 $Comments = "The following VMs require remediation to mitigate the Spectre vulnerability. See the following URLs for more information: <a href='https://knowledge.broadcom.com/external/article?legacyId=52085' target='_blank'>KB 52085</a>, <a href='https://www.virtuallyghetto.com/2018/01/verify-hypervisor-assisted-guest-mitigation-spectre-patches-using-powercli.html' target='_blank'>Virtually Ghetto</a>."
 $Display = "Table"
 $Author = "William Lam"
-$PluginVersion = 1.1
+$PluginVersion = 1.4
 $PluginCategory = "vSphere"
-
-
-# Changelog
-## 1.0 : Initial version.
-## 1.2 : The variable $vm has been changed to $vmobj because $vm is defined globally. This modification allows subsequent plugins to run without problems.
-## 1.3 : Treat vHW >= 9 as capable; only flag VMs missing IBRS/IBPB/STIBP. Older vHW reported as N/A.
 
 # Changelog
 ## 1.0 : Initial version.
 ## 1.2 : The variable $vm has been changed to $vmobj because $vm is defined globally. This modification allows subsequent plugins to run without problems.
 ## 1.3 : Treat vHW >= 9 as capable; only flag VMs missing IBRS/IBPB/STIBP. Older vHW reported as N/A.
 ## 1.4 : Add SSBD/SSB_NO detection (Spectre v2 SSB mitigation) and include KB 317619 reference.
-
