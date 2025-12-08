dcf9eac88d0fe8de52c060574bb85a39f0803a3c
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2025-12-08 01:51:01 -0500

    Update VM CPU %RDY script to version 1.4. Fix CPU ready calculation to utilize actual sample interval for vSphere 8 realtime data, enhancing accuracy. Add MaxSamples parameter for improved performance monitoring.

--- Plugins/60 VM/32 VM CPU Percent RDY.ps1
+++ Plugins/60 VM/32 VM CPU Percent RDY.ps1
@@ -1,8 +1,8 @@
 $Title = "VM CPU %RDY"
 $Comments = "The following VMs have high CPU RDY times, this can cause performance issues. For more information see <a href='https://knowledge.broadcom.com/external/article/387750' target='_blank'>Understanding the CPU Ready values in the vSphere Client</a> and <a href='https://docs.vmware.com/en/VMware-vSphere/8.0/vsphere-performance/index.html' target='_blank'>Performance Best Practices for VMware vSphere 8.0</a>"
 $Display = "Table"
-$Author = "Alan Renouf"
-$PluginVersion = 1.3
+$Author = "Alan Renouf and Jonathan Pitre"
+$PluginVersion = 1.4
 $PluginCategory = "vSphere"
 
 # Start of Settings 
@@ -27,14 +27,20 @@ $i = 0
 ForEach ($v in ($VM | Where-Object { $_.PowerState -eq "PoweredOn" })) {
    Write-Progress -ID 2 -Parent 1 -Activity $plang.pluginActivity -Status $v.Name -PercentComplete ((100 * $i) / $VM.Count)
    For ($cpunum = 0; $cpunum -lt $v.NumCpu; $cpunum++) {
-      $PercReady = [Math]::Round((($v | Get-Stat -ErrorAction SilentlyContinue -Stat Cpu.Ready.Summation -Realtime | Where-Object { $_.Instance -eq $cpunum } | Measure-Object -Property Value -Average).Average) / 200, 1)
-      
-      if ($PercReady -gt $PercCPUReady) {
-         New-Object -TypeName PSObject -Property @{
-            VM        = $v.Name
-            VMHost    = $v.VMHost
-            CPU       = $cpunum
-            PercReady = $PercReady
+      $samples = $v | Get-Stat -ErrorAction SilentlyContinue -Stat cpu.ready.summation -Realtime -MaxSamples 18 | Where-Object { $_.Instance -eq $cpunum }
+      if ($samples) {
+         $avgReadyMs = ($samples | Measure-Object -Property Value -Average).Average
+         $intervalSec = ($samples | Select-Object -First 1 -ExpandProperty IntervalSecs)
+         if (-not $intervalSec -or $intervalSec -eq 0) { $intervalSec = 20 } # default realtime rollup
+         $PercReady = [Math]::Round(($avgReadyMs / ($intervalSec * 10)), 1) # ms over interval -> percent
+         
+         if ($PercReady -gt $PercCPUReady) {
+            New-Object -TypeName PSObject -Property @{
+               VM        = $v.Name
+               VMHost    = $v.VMHost
+               CPU       = $cpunum
+               PercReady = $PercReady
+            }
          }
       }
    }
@@ -47,3 +53,4 @@ $Header = ("VM CPU % RDY over {0}: [count]" -f $PercCPUReady)
 # Change Log
 ## 1.2 :  Added Get-vCheckSetting, code refactor
 ## 1.3 : Updated broken documentation link to official Broadcom Knowledge Base article and Performance Best Practices guide
+## 1.4 : Fix CPU ready calculation to use actual sample interval (vSphere 8 realtime), add MaxSamples
