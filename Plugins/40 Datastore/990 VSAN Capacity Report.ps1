b6356a5ca3d071184381986a96d98b4a0f3ce893
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2025-12-08 01:00:48 -0500

    Update VSAN Capacity Report.ps1 to version 1.2. Raise free-space alert threshold to 25% in line with vSAN operational guidance and enhance comments with a link to relevant documentation for better user understanding.

--- Plugins/40 Datastore/990 VSAN Capacity Report.ps1
+++ Plugins/40 Datastore/990 VSAN Capacity Report.ps1
@@ -1,30 +1,31 @@
 $Title = "VSAN Datastore Capacity"
-$Comments = "VSAN Datastore Capacity Report - Modified version from Alan Renouf & Jonathan Medd's Datastore Report"
+$Comments = "Uses a 25% free-space alert aligned with vSAN operations guidance for rebuild/slack space (see: <a href='https://blogs.vmware.com/cloud-foundation/2021/12/11/revisiting-vsans-free-capacity-recommendations/' target='_blank'>Revisiting vSAN’s Free Capacity Recommendations</a>)."
 $Display = "Table"
-$Author = "William Lam"
-$PluginVersion = 1.1
+$Author = "William Lam, Alan Renouf & Jonathan Medd"
+$PluginVersion = 1.2
 $PluginCategory = "vSphere"
 
 # Start of Settings 
-# Set the warning threshold for VSAN Datastore % Free Space
-$DatastoreSpace = 15
+# Set the warning threshold for VSAN Datastore % Free Space (operational best-practice, not a hard maximum)
+$DatastoreSpace = 25
 # End of Settings
 
 # Update settings where there is an override
 $DatastoreSpace = Get-vCheckSetting $Title "DatastoreSpace" $DatastoreSpace
 
-$Datastores | Where-Object {$_.Type -match 'vsan'} | Select-Object Name, Type, 
-   @{N="CapacityGB";E={[math]::Round($_.CapacityGB,2)}},
-   @{N="ProvisionedGB";E={([math]::Round($_.CapacityGB,2) - [math]::Round($_.FreeSpaceGB,2))}},
-   @{N="FreeSpaceGB";E={[math]::Round($_.FreeSpaceGB,2)}}, PercentFree| Sort-Object PercentFree| Where-Object { $_.PercentFree -lt $DatastoreSpace }
+$Datastores | Where-Object { $_.Type -match 'vsan' } | Select-Object Name, Type, 
+@{N = "CapacityGB"; E = { [math]::Round($_.CapacityGB, 2) } },
+@{N = "ProvisionedGB"; E = { ([math]::Round($_.CapacityGB, 2) - [math]::Round($_.FreeSpaceGB, 2)) } },
+@{N = "FreeSpaceGB"; E = { [math]::Round($_.FreeSpaceGB, 2) } }, PercentFree | Sort-Object PercentFree | Where-Object { $_.PercentFree -lt $DatastoreSpace }
 
 $Header = "VSAN Datastores (Less than $DatastoreSpace% Free) : [count]"
 
-$TableFormat = @{"PercentFree" = @(@{ "-le 15"     = "Row,class|warning"; },
-                                   @{ "-le 10"     = "Row,class|critical" });
-                 "CapacityGB"  = @(@{ "-lt 499.75" = "Cell,style|background-color: #FFDDDD"})
-                }
+$TableFormat = @{"PercentFree" = @(@{ "-le 15" = "Row,class|warning"; },
+      @{ "-le 10" = "Row,class|critical" })
+   "CapacityGB"                = @(@{ "-lt 499.75" = "Cell,style|background-color: #FFDDDD" })
+}
                 
 # Change Log
 ## 1.0 : Initial version
 ## 1.1 : Code refactor
+## 1.2 : Raise free-space alert to 25% and link to vSAN free capacity guidance blog
