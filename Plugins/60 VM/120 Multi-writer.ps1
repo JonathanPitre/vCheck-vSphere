b908d012d41e5ab1d9b6d1204ff2a2346562881c
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2025-12-07 19:07:30 -0500

    Update Multi-writer.ps1 to version 1.2. Enhanced comments with updated links to Broadcom KB for multi-writer parameter information and improved formatting for better readability. Adjusted code for consistency in object property access.

--- Plugins/60 VM/120 Multi-writer.ps1
+++ Plugins/60 VM/120 Multi-writer.ps1
@@ -1,18 +1,18 @@
 $Title = "Multi-writer"
 $Header = "VMs with Multi-writer parameter: [count]"
-$Comments = "The following VMs have multi-writer parameter. A problem will occur in case of svMotion without reconfiguration of the applications which are using these virtual disks and also change of the VM configuration concerned. More information <a href='http://kb.vmware.com/selfservice/microsites/search.do?language=en_US&cmd=displayKC&externalId=1034165'>here</a>."
+$Comments = "The following VMs have multi-writer parameter. A problem will occur in case of svMotion without reconfiguration of the applications which are using these virtual disks and also change of the VM configuration concerned. More information <a href='https://knowledge.broadcom.com/external/article?legacyId=1034165' target='_blank'>Broadcom KB 1034165</a>."
 $Display = "Table"
 $Author = "Petar Enchev, Luc Dekens"
-$PluginVersion = 1.1
+$PluginVersion = 1.2
 $PluginCategory = "vSphere"
 
 # Start of Settings
 # End of Settings
 
 # Multi-writer parameter
-ForEach ($mwvm in $FullVM){
-    $mwvm.Config.ExtraConfig | Where-Object {$_.Key -like "scsi*sharing"} |
-    Select-Object @{N="VM";E={$mwvm.Name}},Key,Value
+ForEach ($mwvm in $FullVM) {
+    $mwvm.Config.ExtraConfig | Where-Object { $_.Key -like "scsi*sharing" } |
+    Select-Object @{N = "VM"; E = { $mwvm.Name } }, Key, Value
 }
 
 # Changelog
