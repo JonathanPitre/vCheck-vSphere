34c70d4374fe2c619de2b19fed360f2d1a2b112d
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2025-12-07 19:19:54 -0500

    Update 45 VMs needing snapshot consolidation.ps1 to reflect the latest VMware documentation link in comments for improved accuracy and user guidance.

--- Plugins/60 VM/45 VMs needing snapshot consolidation.ps1
+++ Plugins/60 VM/45 VMs needing snapshot consolidation.ps1
@@ -1,6 +1,6 @@
 $Title = "VMs needing snapshot consolidation"
 $Header = "VMs needing snapshot consolidation [count]"
-$Comments = "The following VMs have snapshots that failed to consolidate. See <a href='http://blogs.vmware.com/vsphere/2011/08/consolidate-snapshots.html' target='_blank'>this article</a> for more details"
+$Comments = "The following VMs have snapshots that failed to consolidate. See <a href='https://techdocs.broadcom.com/us/en/vmware-cis/vsphere/vsphere/6-7/vsphere-virtual-machine-administration-guide-6-7/managing-virtual-machines/using-snapshots-to-manage-virtual-machines/consolidate-snapshots.html' target='_blank'>this article</a> for more details"
 $Display = "Table"
 $Author = "Luc Dekens, Frederic Martin"
 $PluginVersion = 1.3
@@ -10,5 +10,5 @@ $PluginCategory = "vSphere"
 # End of Settings 
 
 $htabHostVersion = @{}
-$HostsViews | Foreach-Object {$htabHostVersion.Add($_.MoRef,$_.config.product.version)}
-$FullVM | Where-Object {$htabHostVersion[$_.runtime.host].Split('.')[0] -ge 5 -and $_.runtime.consolidationNeeded} | Sort-Object -Property Name | Select-Object Name,@{N="Consolidation needed";E={$_.Runtime.consolidationNeeded}}
+$HostsViews | Foreach-Object { $htabHostVersion.Add($_.MoRef, $_.config.product.version) }
+$FullVM | Where-Object { $htabHostVersion[$_.runtime.host].Split('.')[0] -ge 5 -and $_.runtime.consolidationNeeded } | Sort-Object -Property Name | Select-Object Name, @{N = "Consolidation needed"; E = { $_.Runtime.consolidationNeeded } }
